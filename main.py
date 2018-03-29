ceecd5bea4fc9207cade10c2583e4118172eb8e6
Jenelle Basinger <jenellebas@yahoo.com>
2018-03-28 21:09:10 -0500

    made all-valid.html form

--- main.py
+++ main.py
@@ -16,7 +16,30 @@ def validate_username():
     verify_password = request.form['verify-password']
     email = request.form['email']
 
-    
+
+#if user enters nothing in box and tries to submit it.
+    if username == "":
+        username_error = "You must enter a Username."
+        username = ""
+    #else: #if username is less than 3 characters  or more than 20
+        #if len(username) < 3 or len(username) > 20:
+            #username_error = "That is not a valid username."
+            #username = ""
+        
+
+
+    if password == "":
+        error = "You must enter a Password."
+        return redirect("/?error=" + error)
+    if verify_password == "":
+        error = "Passwords don't match."
+        return redirect("/?error=" + error)
+
+#if all user info is correct:
+@app.route('/all-valid')
+def all_valid():
+    username = request.args.get('username')
+    return '<h1>Welcome, {0}!</h1>'.format(username)
 
 
 app.run()
